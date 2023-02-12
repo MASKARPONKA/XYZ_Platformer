0d6ada54cb00e26044cc8c7f3e91f9ec889631cc
MASKARPONKA <danya_vas_2013@mail.ru>
2023-02-12 21:48:53 +0300

    lesson-2

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private float _direction;
+    [SerializeField] float _speed;
+   public void SetDirection(float direction)
+    {
+        _direction = direction;
+    }
+    private void Update()
+    {
+        if (_direction != 0)
+        {
+            var delta = _direction * _speed * Time.deltaTime;
+            var NewXPosition = transform.position.x + delta;
+            transform.position = new Vector3(NewXPosition, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Hero _hero;
+    private void Awake()
+    {
+        _hero = GetComponent<Hero>();
+    }
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _hero.SetDirection(-1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _hero.SetDirection(1);
+        }
+        else
+        {
+            _hero.SetDirection(0);
+        }
+    }
+}
